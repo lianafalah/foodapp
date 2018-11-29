18149cf0f2c2b20c619cfe5f76f87e75b499c45d
Liana Falah <falahpujimarliana@gmail.com>
2018-11-29 16:10:36 +0700

    no message

--- app/src/main/java/com/example/foodapps/AddFood.java
+++ app/src/main/java/com/example/foodapps/AddFood.java
@@ -1,13 +1,44 @@
 package com.example.foodapps;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class AddFood extends AppCompatActivity {
 
+    private ImageButton foodImage;
+    private static final int GALLREQ=1;
+    private EditText name,desc,price;
+    private Uri uri = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_food);
+
+        name = (EditText) findViewById(R.id.itemName);
+        desc = (EditText) findViewById(R.id.itemDesc);
+        price = (EditText) findViewById(R.id.itemPrice);
+    }
+
+    public void imageButtonClick(View view){
+        Intent galeryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galeryIntent.setType("image/*");
+        startActivityForResult(galeryIntent,GALLREQ);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==GALLREQ && requestCode==RESULT_OK){
+             uri=data.getData();
+             foodImage=(ImageButton)findViewById(R.id.foodImageButton);
+             foodImage.setImageURI(uri);
+        }
     }
 }
--- app/src/main/java/com/example/foodapps/MainActivity.java
+++ app/src/main/java/com/example/foodapps/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.foodapps;
 
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+    public void addFoodButtonClicked(View view){
+        Intent addFoodIntent= new Intent(MainActivity.this,AddFood.class);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.manu.menu_main, menu);
+        return true;
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        //if(id==R.id.action)''
+        return super.onOptionsItemSelected(item);
     }
 }
